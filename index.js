81aebf9e1b3cb9ad9c9c09ec8203cd690f261042
Souparno <souparna2000shee@gmail.com>
2024-06-20 04:11:49 +0530

    change

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://neox-invoice-system.netlify.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }))
